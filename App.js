553b8d356de05f90b95f304e9b3812f3cc6512aa
carlos vilca <carlosvilca.juy@gmail.com>
2023-04-09 21:43:25 -0300

    se agrego: imagenes, coponente location-list, data, estilos, Pressable, Flatlist

--- App.js
+++ App.js
@@ -4,15 +4,38 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { HomeScreen } from './src/screens/home/HomeScreen';
 import { ProfileScreen } from './src/screens/profile/ProfileScreen';
+import { Ionicons } from '@expo/vector-icons';
+import { COLORS, SPACING } from './src/utils/theme'
+import { LocationListScreen } from './src/screens/location-list/LocationListScreen';
 
 const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Home: 'home',
+  Profile: 'md-person-circle',
+  Explorar: 'earth'
+}
+
+const screenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name]//TAB_ICON[Home]
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+    tabBarActiveTintColor: COLORS.primary,
+    tabBarInactiveTintColor: COLORS.inactive,
+    headerShown: false,
+    tabBarStyle: styles.tabBar
+  }
+}
 
 export default function App() {
   return (
     <>
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator screenOptions={screenOptions}>
           <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Explorar" component={LocationListScreen} />
           <Tab.Screen name="Profile" component={ProfileScreen} />
         </Tab.Navigator>
       </NavigationContainer>
@@ -29,3 +52,11 @@ export default function App() {
 //     justifyContent: 'center',
 //   },
 // });
+
+const styles = StyleSheet.create({
+  tabBar:{
+    height: SPACING.xxxl,
+    paddingBottom: SPACING.xs,
+    paddingTop: SPACING.xs
+  }
+})
